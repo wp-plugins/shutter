0ab362b01d24177b4b8e563508b6a578221d0542
scottbasgaard <scottbasgaard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-07 14:41:19 +0000

    tagging 0.2
    git-svn-id: https://plugins.svn.wordpress.org/shutter/tags/0.2.0@568671 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/gallery/images.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Single Gallery Images
+ */
+?>
+
+<?php global $post, $wpshutter; ?>
+
+<ul class="shutter-gallery">
+	<?php	
+	$attachments = get_posts( array(
+		'post_type' 	=> 'attachment',
+		'numberposts' 	=> -1,
+		'post_status' 	=> null,
+		'post_parent' 	=> $post->ID,
+		'post_mime_type'=> 'image',
+		'orderby'		=> 'menu_order',
+		'order'			=> 'ASC'
+	) );
+		
+	if ($attachments) {
+		
+		$loop = 0;
+		$columns = apply_filters( 'shutter_gallery_thumbnails_columns', 3 );
+		
+		foreach ( $attachments as $key => $attachment ) {
+			
+			$classes = array( 'shutterbox' );
+			$order = '';
+			
+			if ( $loop == 0 || $loop % $columns == 0 ) 
+				$order = ' first';
+			
+			if ( ( $loop + 1 ) % $columns == 0 ) 
+				$order = ' last';
+			
+			echo '<li class="gallery-image'.$order.'" >';
+			
+			$attachment_url = wp_get_attachment_image_src( $attachment->ID, 'shutter-gallery-lightbox' );
+			printf( '<a href="%s" title="%s" rel="thumbnails" class="%s">%s</a>', $attachment_url[0], esc_attr( $attachment->post_title ), implode(' ', $classes), wp_get_attachment_image( $attachment->ID, apply_filters( 'gallery_thumbnail_size', 'shutter-gallery-thumb' ) ) );
+			
+			echo '</li>';
+			
+			$loop++;
+
+		}
+		
+	}
+?>
+</ul>
