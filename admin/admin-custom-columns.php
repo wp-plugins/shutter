909a7a506542cc8c26eaceeb8daf0bb01317d67f
scottbasgaard <scottbasgaard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-10 14:21:05 +0000

    tagging 0.4
    git-svn-id: https://plugins.svn.wordpress.org/shutter/tags/0.4.0@570095 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/admin-custom-columns.php
@@ -0,0 +1,43 @@
+<?php
+
+function shutter_gallery_columns( $columns ) {
+	$new_columns = array();
+	$new_columns['cb'] = $columns['cb'];
+	$new_columns['gallery-thumb'] = __('Image', 'woocommerce');
+	unset($columns['cb']);
+	$columns = array_merge( $new_columns, $columns );
+	unset($columns['comments']); // Remove Comments Column
+	$columns['gallery-count'] = __('Count', 'woocommerce');
+	return $columns;
+}
+add_filter('manage_wps-gallery_posts_columns', 'shutter_gallery_columns');
+ 
+function shutter_gallery_column( $column ) {
+	
+	global $wpshutter;
+	global $post;
+	
+	if ( $column == 'gallery-thumb' ) :
+		
+		$edit_link = get_edit_post_link( $post->ID );
+    	
+    	if ( has_post_thumbnail($post->ID) ) :
+				
+			echo '<a class="row-title" href="'.$edit_link.'">'.get_the_post_thumbnail($post->ID, 'shutter-custom-column-thumb').'</a>';
+				
+		else :
+				
+			echo '<a class="row-title" href="'.$edit_link.'"><img src="'.$wpshutter->plugin_url().'/images/placeholder.png" alt="Placeholder" width="100" height="100" /></a>';
+				
+		endif;
+		
+	elseif ( $column == 'gallery-count' ) :
+		
+		$attachment_count = count(get_children( array( 'post_parent' => $post->ID ) ));
+ 		echo $attachment_count;
+	
+	endif;
+ 	
+	return $column;	
+}
+add_filter('manage_wps-gallery_posts_custom_column', 'shutter_gallery_column');
---
+++ shutter-hooks.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Shutter Hooks
+ *
+ * Action/filter hooks used for Shutter function and templates
+ *
+ */
+ 
+if ( !is_admin() || defined('DOING_AJAX') ) :
+
+// Wrappers
+add_action( 'gallery_before_main_content', 'shutter_output_content_wrapper', 10);
+add_action( 'gallery_after_main_content', 'shutter_output_content_wrapper_end', 10);
+
+/* Sidebar */
+add_action( 'get_sidebar', 'shutter_prevent_sidebar_cache' );
+add_action( 'shutter_sidebar', 'shutter_get_sidebar', 10);
+
+// Single Gallery
+add_action( 'shutter_before_single_gallery', 'shutter_template_single_title', 5);
+add_action( 'shutter_before_single_gallery', 'shutter_show_gallery', 20);
+
+endif;
